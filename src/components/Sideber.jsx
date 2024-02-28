3934a3ab04a301cc889630670e2c3a134dc77801
Teslim <yusuftessy2005@gmail.com>
2024-02-28 23:51:07 +0100

    Update UI with CSS changes

--- src/components/Sideber.jsx
+++ src/components/Sideber.jsx
@@ -2,17 +2,23 @@ import { NavLink, Outlet } from 'react-router-dom';
 import React from 'react';
 import '../index.css'
 import profile from '../assets/react.svg'
-import { IoLogOutOutline } from "react-icons/io5";
-import { IoIosNotificationsOutline } from "react-icons/io";
+import { IoLogOutOutline, IoSettingsOutline } from "react-icons/io5";
+import { IoIosNotificationsOutline, IoIosSearch } from "react-icons/io";
+import { GrSchedules } from "react-icons/gr";
+import { HiOutlineHome } from "react-icons/hi";
+import { PiHeadset } from "react-icons/pi";
+
 function Sidebar() {
+
     const navData = [
-        { 'path': "/user", "name": "Home" },
-        { "path": "/search", "name": "Search" },
-        { "path": "/schedule", "name": "Schedule" },
-        { "path": "/contact", "name": "Contact" },
-        { "path": "/helpCenter", "name": "Help Center" },
+        { path: "/user", name: "Home", "icon": HiOutlineHome, },
+        { path: "/schedule", name: "Schedule", "icon": GrSchedules },
+        { path: "/search", name: "Search", "icon": IoIosSearch },
+        { path: "/contact", name: "Settings", "icon": IoSettingsOutline },
+        { path: "/helpCenter", name: "Help Center", "icon": PiHeadset },
 
     ];
+
     return (
         <>
             {/* <div className="col-12 row">
@@ -56,31 +62,46 @@ function Sidebar() {
                     </div>
                     <div class="position-sticky pt-2">
                         <ul class=" nav nav-pills flex-column border border-warning p-2">
-                            {navData.map((nav, index) => (<li class="nav-item  mt-1 mb-1  rounded" key={index}>
-                                <NavLink className='nav-link rounded' to={nav.path}>{nav.name}</NavLink>
+                            {navData.slice(0, 3).map((nav, index) => (<li class="nav-item  mt-1 mb-1 border border-info rounded" key={index}>
+
+                                <NavLink className='nav-link rounded border border-danger pt-3 pb-3' to={nav.path}>{<nav.icon size={20} className='me-2' />} {nav.name}</NavLink>
                             </li>))}
                         </ul>
+                        <div className="">
 
-                        <ul class="nav flex-column mb-2 ">
-                            <div class="d-flex align-items-center p-3 border border-success">
-                                <div class="flex-shrink-0">
-                                    <img src={profile} alt="profile" className='rounded-circle border border-danger' />
-                                </div>
-                                <div class="flex-grow-1 d-flex align-items-center ms-1">
-                                    <p className='me-3 mb-0'>Adeyemi Rofiat</p>
-                                    <IoLogOutOutline />
-                                </div>
-                            </div>
+                            <ul class=" nav nav-pills flex-column border border-warning p-2 ">
+                                {navData.slice(3, 5).map((nav, index) => (
+                                    <li class="nav-item  mt-1 mb-1 border border-info rounded" key={index}>
+                                        <NavLink className='nav-link rounded border border-danger pt-3 pb-3' to={nav.path}>
+                                            {<nav.icon size={20} className='me-2' />} {nav.name}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
 
-                        </ul>
+
+                            <ul class="nav flex-column mb-2 profile-section">
+                                <div class="d-flex align-items-center p-3 border border-success">
+                                    <div class="flex-shrink-0">
+                                        <img src={profile} alt="profile" className='rounded-circle border border-danger' />
+                                    </div>
+                                    <div class="flex-grow-1 d-flex align-items-center ms-1">
+                                        <p className='me-3 mb-0'>Adeyemi Rofiat</p>
+                                        <IoLogOutOutline size={30}/>
+                                    </div>
+                                </div>
+
+                            </ul>
+                        </div>
                     </div>
                 </nav>
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 " style={{backgroundColor: '#F9FAFB'}}>
-                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom sticky-top " style={{backgroundColor: '#F9FAFB'}}>
+                
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 no-overflow" style={{ backgroundColor: '#F9FAFB', maxWidth: '83.3333%'}}>
+                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom sticky-top " style={{ backgroundColor: '#F9FAFB' }}>
                         <h2 style={{ fontFamily: 'Outfit', fontSize: '28px', fontWeight: '700', lineHeight: '33.6px', color: '#645D5D' }} className='ps-2'>Swift Rails</h2>
                         <div class="d-flex align-items-center justify-content-between p-2 border border-success">
                             <div class="flex-grow-1 d-flex align-items-center me-4 rounded-circle border border-muted p-2">
-                                <IoIosNotificationsOutline />
+                                <IoIosNotificationsOutline size={30}/>
                             </div>
                             <div class="flex-shrink-0">
                                 <img src={profile} alt="profile" className='rounded-circle border border-danger' />
--- src/pages/userDashboard.jsx
+++ src/pages/userDashboard.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import train from '../assets/Train.png';
+import '../index.css'
 function UserDashboard() {
     return (
         <>
             <div className="container-fluid border border-warning">
                 <div className="container border border-success bg-dark text-white m-3 rounded d-flex justify-content-between align-items-center">
-                    <div className="border border-white m-5 ">
+                    <div className=" m-5 ">
 
-                        <p style={{ fontFamily: 'Outfit', fontSize: '20px', fontWeight: '500', lineHeight: '24px', color: '#ffffff' }}>Get 20% discount on your <br />first trip </p>
-                        <button className="btn btn-light pe-4 ps-4 pt-3 pb-3 mt-4">Book Now</button>
+                        <p style={{ fontFamily: 'Outfit', fontSize: '20px', fontWeight: '500', lineHeight: '24px', color: '#ffffff' }}>Get 20% discount on your <br />first trip! </p>
+                        <button className="btn btn-light pe-4 ps-4 pt-3 pb-3 mt-4" style={{fontFamily: 'inter', fontSize: '12px', fontWeight:'600', lineHeight: '17.4px', textAlign: 'center'}}>Book Now</button>
                     </div>
-                    <div className="border border-white m-5">
-                        <img src={train} alt="tain" />
+                    
+                  <img src={train} alt="tain" className="overlap"/>
                        
-                    </div>
+                    
                 </div>
             </div>
+
+            
         </>
     );
 }
